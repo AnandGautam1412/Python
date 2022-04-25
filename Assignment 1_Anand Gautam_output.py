e22c4c54f0545f8dea8eed26e1e85931dfc6d073
AnandGautam1412 <104383379+AnandGautam1412@users.noreply.github.com>
2022-04-25 23:56:34 +0530

    Add files via upload

---
+++ Assignment 1_Anand Gautam_output.py
@@ -0,0 +1,56 @@
+#output 1
+
+#Enter 1st number : 7
+#Enter 2nd number : 7
+#Enter 3rd number : 7
+#Average of the numbers :  7.0
+
+
+
+#output 2
+
+#Enter Gross Income : 500000
+#Enter the number of dependents :3
+#Tax to be paid :  96200.0
+
+
+#output 3
+
+#Enter the name of the person : abc
+#Enter the age of the person : 16
+#Enter the marks obatined by the person : 89.8
+#Enter the name of the course peson wants to take : civil
+#Enter the SID of the students : 2110215675
+#Information about the person : ['abc', 16, 89.8, 'civil', 2110215675]
+
+
+
+#output 4
+
+#Enter the marks of 1st student : 94
+#Enter the marks of 2nd student : 75
+#Enter the marks of 3rd student : 78
+#Enter the marks of 4th student : 35
+#Enter the marks of 5th student : 41
+#Marks of the students :  [94, 75, 78, 35, 41]
+#Marks of the students in the sorted manner :  [35, 41, 75, 78, 94]
+
+
+
+
+#output 5 a
+
+#['Red', 'Green', 'White', 'Pink', 'Yellow']
+
+
+b1=['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+b1=["Purple" if(i=="Black")or(i=="Pink")else i for i in b1]
+print(b1)
+
+
+#Output 5 b
+
+#['Red', 'Green', 'White', 'Purple', 'Purple', 'Yellow']
+
+
+
